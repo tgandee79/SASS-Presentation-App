809bd9d11c0ab8a5297f6198e3b3380f22a628e7
Todd Gandee <tgandee@bignerdranch.com>
2013-06-17 23:16:25 -0400

    first update, getting sass to work properly from twitter bootstrap.
    Had to switch to sass-bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -148,8 +148,8 @@ module.exports = function (grunt) {
         },
         compass: {
             options: {
-                sassDir: '<%= yeoman.app %>/styles',
-                cssDir: '.tmp/styles',
+                sassDir: '<%= yeoman.app %>/styles/scss',
+                cssDir: '<%= yeoman.app %>/styles/css',
                 generatedImagesDir: '.tmp/images/generated',
                 imagesDir: '<%= yeoman.app %>/images',
                 javascriptsDir: '<%= yeoman.app %>/scripts',
